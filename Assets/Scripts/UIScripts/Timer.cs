000ebc992c6b46ed465a3d5c8ee3bc9bce26b8ef
Jado <jade.bouiges@edu.itescia.fr>
2023-10-22 18:11:51 +0200

    add timer

---
+++ Assets/Scripts/UIScripts/Timer.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Timer : MonoBehaviour
+{
+
+    [SerializeField] bool isGameFinished;
+    public float seconds;
+    private bool isRunning = false;
+    public TMP_Text scoreText;
+
+    private PlayerHealth _playerHealth;
+    public static Timer instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de GameManager dans la scène");
+            return;
+        }
+
+        instance = this;
+        _playerHealth = FindObjectOfType<Player>().GetComponent<PlayerHealth>();
+
+    }
+    void Start()
+    {
+        seconds = 0;
+        StartTimer();
+    }
+    void Update()
+    {
+        if (isRunning && isGameFinished == false)
+        {
+            IncreaseTimer();
+        }
+    }
+
+    void StartTimer()
+    {
+        isRunning = true;
+    }
+
+    void IncreaseTimer()
+    {
+        if (_playerHealth.isAlive)
+        {
+            seconds += Time.deltaTime;
+            //decompte
+            //seconds -= Time.deltaTime;
+            scoreText.text = seconds.ToString();
+            float minute = Mathf.FloorToInt(seconds / 60);
+            float sec = Mathf.FloorToInt(seconds % 60);
+            if (sec < 10)
+            {
+                scoreText.text = minute + " :0" + sec.ToString();
+            }
+            else
+            {
+                scoreText.text = minute + " : " + sec.ToString();
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+
+
+}
